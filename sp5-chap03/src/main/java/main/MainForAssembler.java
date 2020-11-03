1c5cad3d72b1bb36d1e458d14aa9b7a2f4dc3a06
jisoo <tjwltn999@naver.com>
2020-11-03 21:26:10 +0900

    리스트 3.13 end

--- sp5-chap03/src/main/java/main/MainForAssembler.java
+++ sp5-chap03/src/main/java/main/MainForAssembler.java
@@ -43,66 +43,70 @@ public class MainForAssembler {
 	
 
 
+	private static Assembler assembler = new Assembler();  // Assembler 객체 생성. Assembler사용할 객체가 모두 생성됨.
 
-
-
-	private static Assembler assembler = new Assembler();
-
-	private static void processNewCommand(String[] arg) {   // ---------무슨메소드?????????????
+	private static void processNewCommand(String[] arg) {   // 새로운 회원 정보 생성.
 			if(arg.length != 5) {
 				printHelp();   // 명령어를 잘못 입력한 경우 도움말 출력해주는 메서드.
 				return;   // for문에서 break;한거 와 같은 의미. 원래 void메소드에서는 리턴 값이 없음.
 			}//if문 end
 			
-			MemberRegisterService regSvc = assembler.getMemberRegisterService();
-			RegisterRequest req = new RegisterRequest();
+			MemberRegisterService regSvc = assembler.getMemberRegisterService(); // Assembler 객체 사용1
+			RegisterRequest req = new RegisterRequest(); //회원가입.
 			req.setEmail(arg[1]);
 			req.setName(arg[2]);
 			req.setPassword(arg[3]);
 			req.setConfirmPassword(arg[4]);
 			
-			if(!req.isPasswordEqualToConfirmPassword()) {  // 패스워드가 같지 않으면.
+			if(!req.isPasswordEqualToConfirmPassword()) {  // 입력한 암호 값이 올바르지 않을 때,
 				System.out.println("암호와 확인이 일치하지 않습니다.");
 				return;
 			}//if문 end
 			
 			try { //예외처리.
-				regSvc.regist(req); //--------?????????????????
+				regSvc.regist(req); //새로운 회원 데이터가 보관됨.
 				System.out.println("등록했습니다.");
 				
-			} catch (DuplicateMemberException e) {
+			} catch (DuplicateMemberException e) {   // 동일한 이메일을 가진 회원 데이터가 존재할 때,
 				System.out.println("이미 존재하는 이메일입니다.");
 			} 	
 	}// processNewCommand() end
 
 	
-	private static void processChangeCommand(String[] arg) { //-------------->무슨메소드??????????????????
+	private static void processChangeCommand(String[] arg) { // 암호 변경 기능 메소드.
 		if(arg.length != 4) {
 			printHelp();
 			return;
 		}//if문 end
 		
 		ChangePasswordService changePwdSvc =
-				assembler.getChangePasswordService();   // 패스워드 바꾸는 메소드.
+				assembler.getChangePasswordService();   // 패스워드 바꾸는 메소드. Assembler 객체사용2
 		
 		try { //예외처리.
-			changePwdSvc.changePassword(arg[1],arg[2],arg[3]);
-			System.out.println("암호를 변경했습니다.");
-		} catch (MemberNotFoundException e) {
+			changePwdSvc.changePassword(arg[1],arg[2],arg[3]); // 객체의 암호 변경 실행.
+			System.out.println("암호를 변경했습니다."); 
+		} catch (MemberNotFoundException e) {    //이메일에 해당하는 회원 데이터가 존재하지 않을 때,
 			System.out.println("존재하지 않는 이메일입니다.");
 			
-		} catch (WrongIdPasswordException e) {
+		} catch (WrongIdPasswordException e) {  // 암호가 올바르지 않을 때,
 			System.out.println("이메일과 암호가 일치하지 않습니다.");
 		}
 		
+	}// processChangeCommand() end. 
+	
+	// processNewCommand()에서 MemberRegisterService 객체를 사용함. Assembler에서  MemberRegisterService는 객체를 생성할 때 MemberDao객체를 주입함.
+	// -------> processNewCommand() 실행에 성공하면, MemberDao의 map에 생성된 회원데이터를 추가.
+	// processChangeCommand()에서 ChangePasswordService 객체를 사용함. Assembler에서  ChangePasswordService는 객체를 생성할 때 MemberDao객체를 주입함.
+	// -------> processChangeCommand() 실행에 성공하면, MemberDao의 map에 보관된 회원 데이터의 암호가 변경됨.
+	
+	
+	private static void printHelp() {  // 도움말을 출력하는 메소드.
+		System.out.println();
+		System.out.println("잘못된 명령입니다. 아래 명령어 사용법을 확인하세요.");
+		System.out.println("명령어 사용법:");
+		System.out.println("new 이메일 이름 암호 암호확인");
+		System.out.println("change 이메일 현재비번 변경비번");
+		System.out.println();
+			
 	}
-	
-	
-	private static void printHelp() {
-		
-		
-	}
-
-
-	
 }
