223474c8141f8dd6c2d4d8bd30df03a167647d2f
jisoo <tjwltn999@naver.com>
2020-11-09 19:02:17 +0900

    리스트 3.14 end

--- sp5-chap03/src/main/java/config/AppCtx.java
+++ sp5-chap03/src/main/java/config/AppCtx.java
@@ -17,13 +17,14 @@ public class AppCtx {     // Assembler클래스를 대신하여 스프링을 사
 	
 	@Bean
 	public MemberRegisterService memberRegSvc() {
-		return new MemberRegisterService(memberDao());  //-----?
+		return new MemberRegisterService(memberDao());  // MemberRegsiterService 생성자를호출할 때 memberDao()메서드를 호출.
+		                                                // 즉, memberDao()가 생성한 객체를 MemberRegisterService 생성자를 통해 주입.
 	}
 	
 	@Bean
 	public ChangePasswordService changePwdSvc() {
 		ChangePasswordService pwdSvc = new ChangePasswordService();
-		pwdSvc.setMemberDao(memberDao());
+		pwdSvc.setMemberDao(memberDao());  // 이 메서드는 세터 setMemberDao() 메서드를 이용해서 의존 객체를 주입
 		return pwdSvc;
 	}
 
