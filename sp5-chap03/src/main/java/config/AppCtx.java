4d89e8cb599c0d883511a602768d5d9361eebb4d
jisoo <tjwltn999@naver.com>
2020-11-20 01:13:59 +0900

    리스트 3.18 / AppCtx클래스 파일에 두개 이상의 인자를 받는 생성자를 사용하는 설정 추가.

--- sp5-chap03/src/main/java/config/AppCtx.java
+++ sp5-chap03/src/main/java/config/AppCtx.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 import spring.ChangePasswordService;
 import spring.MemberDao;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
 import spring.MemberRegisterService;
 
 @Configuration  // 이 클래스는 Spring 환경설정과 관련된 클래스이다. 라고 말해주는 애노테이션.
@@ -27,6 +29,18 @@ public class AppCtx {     // Assembler클래스를 대신하여 스프링을 사
 		ChangePasswordService pwdSvc = new ChangePasswordService();
 		pwdSvc.setMemberDao(memberDao());  // 이 메서드는 세터 setMemberDao() 메서드를 이용해서 의존 객체를 주입
 		return pwdSvc;
+	}
+	
+	// 두 개 이상의 인자를 받는 생성자를 사용하는 설정 추가 (p.82)
+	
+	@Bean
+	public MemberPrinter memberPrinter() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	public MemberListPrinter listPrinter() {
+		return new MemberListPrinter(memberDao(),memberPrinter());
 	}
 
 }
