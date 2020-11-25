644b8ae2e9e850e4104b73aa12a6e60df9a8da1a
jisoo <tjwltn999@naver.com>
2020-11-25 19:14:31 +0900

    +설명추가

--- sp5-chap03/src/main/java/spring/MemberRegisterService.java
+++ sp5-chap03/src/main/java/spring/MemberRegisterService.java
@@ -2,10 +2,11 @@ package spring;
 
 import java.time.LocalDateTime;
 
-public class MemberRegisterService {
+//이 클래스는 MemberDao 객체의 selectByEmail() 메서드를 이용해서 동일한 이메일을 가진 회원 데이터가 존재하는지 확인.
+public class MemberRegisterService {  
 	private MemberDao memberDao;
 	
-	// 생성자를 토애 의존 객체를 주입 받음.
+	// 생성자를 통해 의존 객체를 주입 받음.  // public MemberRegisterService = 생성자.  why? 클래스와 이름이 동일한 메서드 이기때문에
 	public MemberRegisterService(MemberDao memberDao) {
 		// 주입 받은 객체를 필드에 해당.
 		this.memberDao = memberDao;
