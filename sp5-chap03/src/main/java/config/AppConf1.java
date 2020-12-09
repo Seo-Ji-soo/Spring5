9b6b1457b0c27491b4bc90149080857c1da1134a
Jisoo_dev <tjwltn999@naver.com>
2020-12-09 22:08:17 +0900

    리스트 3.26 coding end

--- sp5-chap03/src/main/java/config/AppConf1.java
+++ sp5-chap03/src/main/java/config/AppConf1.java
@@ -1,5 +1,24 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.MemberDao;
+import spring.MemberPrinter;
+
+// AppCtx 클래스에서의 빈 설정을 AppConf1, AppConf2의 두개의 클래스로 나눠서 설정.
+
+@Configuration
 public class AppConf1 {
+	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+	
+	@Bean
+	public MemberPrinter memberPrinter() {
+		return new MemberPrinter();
+	}
 
 }
