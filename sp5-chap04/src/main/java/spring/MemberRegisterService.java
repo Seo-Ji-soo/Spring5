e574f263db2d401ba29ef63a3240ce321e763637
Jisoo_dev <tjwltn999@naver.com>
2020-12-21 21:34:51 +0900

    리스트 4.6 end

--- sp5-chap04/src/main/java/spring/MemberRegisterService.java
+++ sp5-chap04/src/main/java/spring/MemberRegisterService.java
@@ -2,14 +2,23 @@ package spring;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 //이 클래스는 MemberDao 객체의 selectByEmail() 메서드를 이용해서 동일한 이메일을 가진 회원 데이터가 존재하는지 확인.
 public class MemberRegisterService {  
+	
+	@Autowired
 	private MemberDao memberDao;
 	
-	// 생성자를 통해 의존 객체를 주입 받음.  // public MemberRegisterService = 생성자.  why? 클래스와 이름이 동일한 메서드 이기때문에
+	/*// 생성자를 통해 의존 객체를 주입 받음.  // public MemberRegisterService = 생성자.  why? 클래스와 이름이 동일한 메서드 이기때문에
 	public MemberRegisterService(MemberDao memberDao) {
 		// 주입 받은 객체를 필드에 해당.
 		this.memberDao = memberDao;
+	}*/
+	
+	// 위에 MemberDao에 @Autowired 어노테이션을 붙여줬음으로 자동으로 주입됨. 따라서 위에 처럼(주석처리 부분)이 아닌 밑에처럼만 적으면 됨.
+	public MemberRegisterService(){
+		
 	}
 	
 	public Long regist(RegisterRequest req) {
