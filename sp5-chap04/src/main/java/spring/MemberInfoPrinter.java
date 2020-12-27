9d9bb81cdd72e39567fc1fb33ac6cb14cbd4f802
Jisoo_dev <tjwltn999@naver.com>
2020-12-28 01:41:19 +0900

    @Autowired어노테이션 붙여진 메서드에 @qualifier 어노테이션을 붙여 의존 주입대상을 한정

--- sp5-chap04/src/main/java/spring/MemberInfoPrinter.java
+++ sp5-chap04/src/main/java/spring/MemberInfoPrinter.java
@@ -1,6 +1,7 @@
 package spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 // 지정한 이메일을 갖는 Member를 찾아서 정보를 콘솔에 출력하는 클래스.
 public class MemberInfoPrinter {
@@ -25,7 +26,11 @@ public class MemberInfoPrinter {
 	public void setMemDao(MemberDao memDao) {
 		this.memDao = memDao;
 	}
+	
+	// AppCtx 파일에서 지정한 한정 값(@Qualifier 어노테이션 붙인 후 지정한 이름 "printer")은
+	// @Autowired 어노테이션에서 자동 주입할 빈을 한정할 때 사용.
     @Autowired
+    @Qualifier("printer")  // AppCtx 파일에서 @Qualifier 어노테이션을 붙인 후 한정 값이 "printer"인 빈을 가져다 자동주입 할거다~~~
 	public void setPrinter(MemberPrinter printer) {
 		this.printer = printer;
 	}
