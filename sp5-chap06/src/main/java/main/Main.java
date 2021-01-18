361a025bd7453d4d11676e7de160f792226c6054
Jisoo_dev <tjwltn999@naver.com>
2021-01-18 21:34:32 +0900

    리스트 6.3

--- sp5-chap06/src/main/java/main/Main.java
+++ sp5-chap06/src/main/java/main/Main.java
@@ -1,34 +1,31 @@
 package main;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;//자바 설정에서 정보를 읽어와 빈 객체를 생성하고 관리.
+import org.springframework.context.support.AbstractApplicationContext;
 
 import config.AppContext;
+import config.AppCtx;
+import spring.Client;
 import spring.Greeter;
 
 // 자세한 설명은 "sp5-chap02"프로젝트 Main.java 파일 참고.
 
 //스프링 컨테이너 클래스. --> 설정 클래스에서 정보를 읽어와 알맞은 빈 객체를 생성하고 각 빈을 연결(의존 주입)하는 작업을 수행.
+//AppCtx를 이용해서 스프링 컨테이너를 생성하고 Client 빈 객체를 구해 사용하는 코드 작성하는 클래스.
 public class Main { 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
-		// 1.컨테이너 초기화
-		// 초기화가 완료 되면 컨테이너를 사용할 수 있음.
-		// 즉, getBean()과 같은 메서드를 이용해서 컨테이너에 보관된 빈 객체를 구한다는 것을 뜻함.
-		AnnotationConfigApplicationContext ctx =   
-				new AnnotationConfigApplicationContext(AppContext.class); 
+		AbstractApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(AppCtx.class);
 		
-		// 2.컨테이너에서 빈 객체를 구해서 사용.
-		Greeter g= ctx.getBean("greeter", Greeter.class); 
-		String msg = g.greet("스프링");
-		System.out.println(msg);
+		Client client = ctx.getBean(Client.class);
+		client.send();
 		
-		// 3. 컨테이너 종료.
-		// close()는 AbstractApplicationContext 클래스에 정의 되어 있음.
-		// 자바 설정을 사용하는 AnnotationConfigApplicationContext 클래스나 
-		// XML설정을 사용하는 GenericXmlApplicationContext 클래스 모두 AbstractApplicationContext 클래스 상속을 받고 있다.
-		// 따라서 close()를 이용해서 컨테이너를 종료 할 수 있다.
 		ctx.close();
+		
 	
 	}
 }
