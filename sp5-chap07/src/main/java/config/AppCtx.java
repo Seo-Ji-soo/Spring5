f75613031e082d0f7335b6238978293576d75969
Jisoo_dev <tjwltn999@naver.com>
2021-01-27 23:57:01 +0900

    프록시 생성방식 (책 p.168) 이해 및 설명 작성.

--- sp5-chap07/src/main/java/config/AppCtx.java
+++ sp5-chap07/src/main/java/config/AppCtx.java
@@ -11,7 +11,7 @@ import chap07.RecCalculator;
 @Configuration
 //@Aspect 어노테이션을 붙인 클래스를 적용하려면 @EnableAspectJAutoProxy 어노테이션 설정 클래스를 붙여야함.
 // 이 어노테이션을 추가하면 스프링은 @Aspect 어노테이션이 붙은 빈 객체를 찾아서 빈 객체의 @Pointcut 설정과 @Around 설정을 사용함.
-@EnableAspectJAutoProxy 
+@EnableAspectJAutoProxy(proxyTargetClass = true) // 빈객체가 인터페이스를 상속할때, 클래스를 이용해서 프록시를 생성. 
 public class AppCtx {
 	
 	@Bean
@@ -19,8 +19,12 @@ public class AppCtx {
 		return new ExeTimeAspect();
 	}
 	
-	@Bean // Calculator 타입이 chap07 패키지에 속함. calculator 빈에 ExeTimeAspect 클래스에 정의한 공통 기능인 measure()를 적용.
-	public Calculator calculator() {
+	
+	
+
+	
+	@Bean // AOP 적용시 RecCalculator가 상속받은 Calculator 인터페이스를 이용해서 프록시 생성.
+	public Calculator calculator() {// Calculator 타입이 chap07 패키지에 속함. calculator 빈에 ExeTimeAspect 클래스에 정의한 공통 기능인 measure()를 적용.
 		return new RecCalculator();
 	}
 
--- sp5-chap07/src/main/java/main/MainAspect.java
+++ sp5-chap07/src/main/java/main/MainAspect.java
@@ -1,8 +1,10 @@
 package main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import chap07.Calculator;
+import chap07.RecCalculator;
 import config.AppCtx;
 
 public class MainAspect {
@@ -11,7 +13,17 @@ public class MainAspect {
 		AnnotationConfigApplicationContext ctx =
 				new AnnotationConfigApplicationContext(AppCtx.class);
 		
+		/* 수정 전
 		Calculator cal = ctx.getBean("calculator",Calculator.class); // AOP를 적용하지 않았으면 여기서 리턴한 객체는 프록시 객체가 아닌 RecCalculator타입.
+		*/
+		
+		// 수정 후 (import에도 RecCalculator 추가)
+		// AppCtx에서 calculator빈을 생성할 때, RecClculator클래스 이므로 문제 없어보이지만 아님. 오류남.
+		// 왜? "calculator" 빈의 실제 타입은 Calculator를 상속한 프록시 타입이므로
+		// RecCalculator로 타입 변환을 할 수 없기 때문에 익셉션 발생.
+		// but, 설정 클래스에서 @EnableAspectJAutoProxy(proxyTargetClass = true)으로 설정을 한다면 아래와 같이(25행) 코드를 작성해도 오류가 안남.
+		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
+		
 		long fiveFact = cal.factorial(5);
 		System.out.println("cal.factorial(5) = "+fiveFact);
 		//이거 출력결과를 보면 Calculator타입이 RecCalculator 클래스가 아니고 $Proxy17이다.
